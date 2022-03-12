791430ed3062004ffc64aa1e95919e1936017fc0
Ronan Laker <ronan.laker@gmail.com>
2022-03-12 18:32:21 +0000

    ✨ deal with timezones

--- Hack/load.py
+++ Hack/load.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from pathlib import Path
 
 import pandas as pd
+import pytz
 
 DATA_DIR = Path("./Data")
 
@@ -15,7 +16,6 @@ class loader(abc.ABC):
     """
 
     def load(self):
-        print(self.local_path)
         df = self.load_csv(self.local_path)
         return self.set_date_index(df)
 
@@ -47,20 +47,28 @@ class systemprice(loader):
     def fname(self):
         return "systemprice.csv"
 
-    def parse_dates(self, date_strs, periods):
-        dates = []
-        for date_str, period in zip(date_strs, periods):
-            date = datetime.strptime(date_str, "%d/%m/%Y")
-            date += timedelta(minutes=30 * period)
-            dates.append(date)
+    def load(self):
+        df = self.load_csv(self.local_path)
+        df = self.set_date_index(df)
+        df = add_local_time(df)
+        return df
 
-        return dates
+    def parse_dates(self, df):
+        utc_dates = []
+        start_date_str = df["Settlement Date"][0]
+        start_date = datetime.strptime(start_date_str, "%d/%m/%Y")
+
+        # just add 30 minutes intervals, and deal with local
+        # time zone later
+        for i in range(df.shape[0]):
+            utc_date = start_date + timedelta(minutes=30 * i)
+            utc_dates.append(utc_date)
+        return utc_dates
 
     def set_date_index(self, df):
-        parsed_dates = self.parse_dates(df["Settlement Date"], df["Settlement Period"])
+        parsed_dates = self.parse_dates(df)
         df["Date"] = parsed_dates
         df = df.set_index("Date")
-        df.drop(columns=["Settlement Date"], inplace=True)
         return df
 
 
@@ -69,9 +77,10 @@ class epex(loader):
     def fname(self):
         return "epex_day_ahead_price.csv"
 
-    def parse_dates(self, date_strs, periods):
+    def parse_dates(self, date_strs):
         dates = []
-        for date_str, period in zip(date_strs, periods):
+        for date_str in date_strs:
+            # this is UTC time
             date = datetime.strptime(date_str[:-6], "%Y-%m-%d %H:%M:%S:")
             dates.append(date)
 
@@ -92,9 +101,10 @@ class spot(loader):
     def fname(self):
         return "spot_intraday_price.csv"
 
-    def parse_dates(self, date_strs, periods):
+    def parse_dates(self, date_strs):
         dates = []
-        for date_str, period in zip(date_strs, periods):
+        for date_str in date_strs:
+            # this is UTC time
             date = datetime.strptime(date_str[:-6], "%Y-%m-%d %H:%M:%S:")
             dates.append(date)
 
@@ -108,3 +118,29 @@ class spot(loader):
         return pd.read_csv(
             fname, delimiter=delimiter, header=header, parse_dates=["timestamp"]
         )
+
+
+def add_local_time(df):
+    local_tz = pytz.timezone("Europe/London")
+    local_timestamps = []
+    local_dates = []
+    for date_index in df.index:
+        date = datetime(
+            date_index.year,
+            date_index.month,
+            date_index.day,
+            date_index.hour,
+            date_index.minute,
+        )
+        date.astimezone(pytz.utc)
+        date_as_tz = date.astimezone(local_tz)
+        local_dates.append(date_as_tz)
+        local_timestamps.append(
+            datetime.strptime(
+                (date_as_tz + date_as_tz.dst()).strftime("%Y-%m-%d %H:%M"),
+                "%Y-%m-%d %H:%M",
+            )
+        )
+    df["local_datetime"] = local_dates
+    df["local_time"] = local_timestamps
+    return df
